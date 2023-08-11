8d9eb634bf79c4908d6f846cbfcd89d8a182d445
louohs <louisa.hadjili@epitech.eu>
2023-08-11 12:11:21 +0200

    [ADD] update

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React from "react";
 import "../Styles/Home.css";
 
 const Footer = () => {
-    return (
-        <>
-            <div className="">
-
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="active title-link">
+          © 2023 Developed, designed by <span className="colors">Louisa</span> |
+          Tous droits réservés.
+      </div>
+    </>
+  );
+};
 
 export default Footer;
--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -1,60 +1,92 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from "react";
+import emailjs from "emailjs-com";
 import "../Styles/Form.css";
+// import Footer from "./Footer";
 
 const Form = () => {
+  const [status, setStatus] = useState("");
+  const [formData, setFormData] = useState({
+    name: "",
+    message: "",
+  });
 
-    const [formData, setFormData] = useState({
-        name: '',
-        message: '',
-    });
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
 
-    const handleInputChange = (event) => {
-        const { name, value } = event.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const emailParams = {
+      to_name: "Louisa",
+      name: formData.name,
+      message: formData.message,
     };
-    const handleSubmit = (event) => {
-        event.preventDefault();
-    };
 
-    return (
-        <>
-            <div className="container">
-                <div className="sub-home">
+    try {
+      const response = await emailjs.send(
+        "SERVICE_LOUOHS_4683939",
+        "template_4fjeb3a",
+        emailParams
+      );
 
-                    <form onSubmit={handleSubmit}>
-                        <div className="form">
-                            <p className="title-form">Écris-moi un joli mot</p>
-                                <label htmlFor="name" className="active title-link">Prénom</label>
-                                <input
-                                    type="text"
-                                    id="name"
-                                    name="name"
-                                    value={formData.name}
-                                    onChange={handleInputChange}
-                                    placeholder="Ton prénom"
-                                />
+      console.log("Email envoyé !", response.status, response.text);
+      setStatus("Ton message a bien été envoyé!");
+    } catch (error) {
+      console.error("Erreur d'envoi d'e-mail :", error);
+    }
+  };
 
+  useEffect(() => {
+    emailjs.init("E-u_BQRxlwXZIR-_Y");
+  }, []);
 
-                                <label htmlFor="message" className="active title-link">Message</label>
-                                <textarea
-                                    id="message"
-                                    name="message"
-                                    value={formData.message}
-                                    onChange={handleInputChange}
-                                    placeholder="If my life is going to mean anything, I have to live it myself."
-                                />
-                        </div>
-                        <div className="button">
-                            <button type="submit" className="button-design">Envoyer</button>
-                        </div>
-                    </form>
-                </div>
+  return (
+    <>
+      <div className="container">
+        <div className="sub-home">
+          <form onSubmit={handleSubmit}>
+            <div className="form">
+              <p className="title-form">Écris-moi un joli mot</p>
+              <label htmlFor="name" className="active title-link">
+                Prénom
+              </label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={formData.name}
+                onChange={handleInputChange}
+                placeholder="Ton prénom"
+              />
+
+              <label htmlFor="message" className="active title-link">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                value={formData.message}
+                onChange={handleInputChange}
+                placeholder="If my life is going to mean anything, I have to live it myself."
+              />
             </div>
-        </>
-    )
-}
+            <div className="button">
+              <button type="submit" className="button-design">
+                Envoyer
+              </button>
+            </div>
+            <div className="success-message">{status && <p>{status}</p>}</div>
+          </form>
+        </div>
+      </div>
+        {/* <Footer/> */}
+    </>
+  );
+};
 
 export default Form;
