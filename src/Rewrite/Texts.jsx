6a8bf025485c49ec501e7b341fa84742a29e5b53
louohs <louisa.hadjili@epitech.eu>
2023-08-17 19:24:37 +0200

    [ADD] TEXTS 3

--- src/Rewrite/Texts.jsx
+++ src/Rewrite/Texts.jsx
@@ -12,18 +12,85 @@ const Texts = () => {
             <div className="sub-image">
               <div className="image"></div>
             </div>
-            <div className="title-texts">Toi</div>
-              <p className="active title-link date-text">16 AOÛT 2023</p>
-              <p className="desc-texts-others">
-              « Il n’y croyait pas en l’amour en ce monde… il croyait en l’amour des cieux. Son seul combat est de se battre, même s’il craignait la mort. Il était un être en marge, un rêveur qui croyait en la force cosmique plutôt qu’en la force terrestre, ses yeux brillaient d’une lueur où il ne percevait que l’univers, au-delà de la réalité visible. »
+            <div className="title-texts">Brises inopinées</div>
+            <p className="active title-link date-text">17 AOÛT 2023</p>
+            <p className="desc-texts">
+              Devrais-tu te tourmenter parce que les moments difficiles te
+              sollicitaient ?<br />
+              Fallait-il que tu te contraignes à croire en dépit du fardeau de
+              tes douleurs ?<br />
+              Te surprenais-tu à fréquenter souvent tes moments de solitude qui
+              te touchaient profondément ?<br />
+              Te sentais-tu vidé dès lors que personne ne te tendait la main ?
+              <br />
+              As-tu déjà changé ton apparence pour faire croire aux autres que
+              tu étais exceptionnel ?<br />
+              T’es-tu déjà interrogé sur ta propre valeur ?<br />
+              Te sentais-tu mal face à la douleur de ceux qui te percevaient
+              comme fort ?<br />
+              Croyais-tu que l’existence ne te préservait pas de l’essence de
+              ton âme ?<br />
+              Cette lutte en valait-elle la peine pour toutes les personnes que
+              tu avais croisées dans ton chemin ?<br />
+              Avais-tu pensé, dans ce bas-monde, avant même ta naissance, que
+              vivre ici ne mènerait qu’à devenir poussière ?<br />
+              Te venait-il à l’esprit que ceux qui jouissaient de cette vie ne
+              se souvenaient pas de ceux qui ne l’avaient jamais connue ?<br />
+              Avais-tu déjà versé des larmes en raison de ton sentiment de vide
+              et de ta vie remplie de mensonge à soi ?<br />
+              <p className="underline">
+                Je pense qu’on est dans le même pétrin.
               </p>
-              <div className="title-texts">Premier éveil, dernier éveil</div>
-              <p className="active title-link date-text">8 AOÛT 2023</p>
-              <p className="desc-texts">
-              À l’heure où je t’ai écrit ces mots, j’ai été submergée par ma peur et mon angoisse. Parmi toutes mes peines que j’ai traversées, je continue à avancer malgré la lourdeur de mes larmes qui se sont mêlées aux tiennes. J’ai appris durant ces mois, qu’exprimer ce que l’on ressent n’est pas facile, encore une fois, je n’ai voulu que t’aider, t’accorder tout l’amour, la douceur dont je suis capable. Connaître des vraies brides qui te sont toi, car la réelle version de toi-même est celle dont tu ne peux saisir qu’à travers tes propres mots : une épreuve que provoque tes sentiments et tes désirs. Tu sais que Celui qui a créé cette vie, n’a pas été créée dans le but de te rendre la vie facile. Que cela soit pour toi ou pour moi. Lorsqu’on a du mal à dire ce que les sentiments nous portent, on remet en question tout ce que l’on a traversé, les âmes pures aident autrui à se comporter : le meilleur d’eux-mêmes. Toi, tu es comme un précieux cadeau que l’on veut apaiser, tu es une âme pure, je tiens à te confier que mon âme ressent le besoin de te revoir, bien que cette absence n’a fait qu’accroître mon sentiment de vide. Ton départ m’a enseigné une grande leçon de vie, car tu m’as rendue encore plus forte, plus éveillée, plus belle, toutes les choses que tu as su me dire et apprises, ont donné sens à une sensation de protection. <br/> En fin de compte, tu as été là sans forcément pouvoir paraître auprès de moi, même absent tes mots résonnent comme le bruissement de l’eau. Je songe à l’idée d’entendre l’écume de ta voix, de tes gestes, enfin de ta bienveillance. Souvent, je me dis que retrouver cette intensité en ce bas-monde est désormais hors de ma portée, si ces jours qui défilent m’ont été les plus éperdus…
-              Alors, nos belles banalités, nos longues conversations d’autrefois perdurent quelque part, dans un endroit où elles peuvent vivre de choses merveilleuses, qu’elles ne connaissent pas ce que nos cœurs éprouvent dans la lueur de nos jours illuminés. Ton éclat me donne la raison de continuer à persévérer, de m’avouer que ma douceur serait-elle peut-être d’une autre destinée. <br/><br/> Je t’embrasse très fort, <br/> Louisa. 
-              </p>
-              {/* <div className="line"></div> */}
+            </p>
+            <div className="title-texts">Toi, ce que tu valais</div>
+            <p className="active title-link date-text">16 AOÛT 2023</p>
+            <p className="desc-texts-others">
+              « Il n’y croyait pas en l’amour en ce monde… il croyait en l’amour
+              des cieux. Son seul combat est de se battre, même s’il craignait
+              la mort. Il était un être en marge, un rêveur qui croyait en la
+              force cosmique plutôt qu’en la force terrestre, ses yeux
+              brillaient d’une lueur où il ne percevait que l’univers, au-delà
+              de la réalité visible. »
+            </p>
+            <div className="title-texts">Premier éveil, dernier éveil</div>
+            <p className="active title-link date-text">8 AOÛT 2023</p>
+            <p className="desc-texts">
+              À l’heure où je t’ai écrit ces mots, j’ai été submergée par ma
+              peur et mon angoisse. Parmi toutes mes peines que j’ai traversées,
+              je continue à avancer malgré la lourdeur de mes larmes qui se sont
+              mêlées aux tiennes. J’ai appris durant ces mois, qu’exprimer ce
+              que l’on ressent n’est pas facile, encore une fois, je n’ai voulu
+              que t’aider, t’accorder tout l’amour, la douceur dont je suis
+              capable. Connaître des vraies brides qui te sont toi, car la
+              réelle version de toi-même est celle dont tu ne peux saisir qu’à
+              travers tes propres mots : une épreuve que provoque tes sentiments
+              et tes désirs. Tu sais que Celui qui a créé cette vie, n’a pas été
+              créée dans le but de te rendre la vie facile. Que cela soit pour
+              toi ou pour moi. Lorsqu’on a du mal à dire ce que les sentiments
+              nous portent, on remet en question tout ce que l’on a traversé,
+              les âmes pures aident autrui à se comporter : le meilleur
+              d’eux-mêmes. Toi, tu es comme un précieux cadeau que l’on veut
+              apaiser, tu es une âme pure, je tiens à te confier que mon âme
+              ressent le besoin de te revoir, bien que cette absence n’a fait
+              qu’accroître mon sentiment de vide. Ton départ m’a enseigné une
+              grande leçon de vie, car tu m’as rendue encore plus forte, plus
+              éveillée, plus belle, toutes les choses que tu as su me dire et
+              apprises, ont donné sens à une sensation de protection. <br /> En
+              fin de compte, tu as été là sans forcément pouvoir paraître auprès
+              de moi, même absent tes mots résonnent comme le bruissement de
+              l’eau. Je songe à l’idée d’entendre l’écume de ta voix, de tes
+              gestes, enfin de ta bienveillance. Souvent, je me dis que
+              retrouver cette intensité en ce bas-monde est désormais hors de ma
+              portée, si ces jours qui défilent m’ont été les plus éperdus…
+              Alors, nos belles banalités, nos longues conversations d’autrefois
+              perdurent quelque part, dans un endroit où elles peuvent vivre de
+              choses merveilleuses, qu’elles ne connaissent pas ce que nos cœurs
+              éprouvent dans la lueur de nos jours illuminés. Ton éclat me donne
+              la raison de continuer à persévérer, de m’avouer que ma douceur
+              serait-elle peut-être d’une autre destinée. <br />
+              <br /> Je t’embrasse très fort, <br /> Louisa.
+            </p>
+            {/* <div className="line"></div> */}
           </div>
         </div>
       </div>
